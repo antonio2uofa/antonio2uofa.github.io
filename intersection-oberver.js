3a06f5b7c6213c7500a14f46d61ddeeca54ab134
antonio2uofa <antonio2@ualberta.ca>
2022-09-21 13:43:19 -0600

    Fades In and Out

--- intersection-oberver.js
+++ intersection-oberver.js
@@ -1,5 +1,7 @@
 const sectionOne = document.querySelector('.trigger');
 
+let visible = false;
+
 const options = {root: null, threshold: 0, rootMargin: "-100px"};
 
 var faders = document.getElementsByClassName('fadeIn');
@@ -7,10 +9,17 @@ var faders = document.getElementsByClassName('fadeIn');
 const observer = new IntersectionObserver(function(entries, observer){
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
-            return;
+            if(!visible) {
+                return;
+            } else {
+                for(let i = 0; i < faders.length; i++) {
+                    faders[i].classList.remove("appear");
+                }
+            };
         } else {
             for(let i = 0; i < faders.length; i++) {
                 faders[i].classList.add("appear");
+                visible = true;
             }
         };
     });
